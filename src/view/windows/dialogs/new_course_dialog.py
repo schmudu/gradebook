cb4035da26f9911fd9fd31af09a56bb62bfaf91a
Patrick Lee <patrick.robin.lee@gmail.com>
2014-11-28 23:47:17 -0800

    Enhanced new course dialog.

--- src/view/windows/dialogs/new_course_dialog.py
+++ src/view/windows/dialogs/new_course_dialog.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QBoxLayout, QFormLayout, QLabel, QLineEdit, QPushButton, QDialog, QWidget
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QBoxLayout, QFormLayout, QLabel, QLineEdit, QPushButton, QDialog, QSizePolicy, QWidget
 
 class NewCourseDialog(QDialog):
   def __init__(self, *args, **kwargs):
@@ -10,17 +11,19 @@ class NewCourseDialog(QDialog):
     
     self.setWindowTitle("New Course")
 
-    self.setFixedSize(300, 200)
+    self.setFixedSize(400, 150)
 
     # main widget
     #main_widget = QWidget()
-    main_layout = QBoxLayout(QBoxLayout.TopToBottom, self)
+    main_layout = QBoxLayout(QBoxLayout.TopToBottom)
 
     # form widget
     form_widget = QWidget()
     form_layout = QFormLayout()
+    form_layout.setFormAlignment(QtCore.Qt.AlignLeft)
     new_course_label = QLabel("Course Name:")
-    new_course_entry = QLineEdit()
+    new_course_entry = QLineEdit("<Enter course name>")
+    new_course_entry.setFixedWidth(230)
     form_layout.addRow(new_course_label, new_course_entry)
     form_widget.setLayout(form_layout)
 
